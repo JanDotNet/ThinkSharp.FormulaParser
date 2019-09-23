6306a32cf45082e9ebed8c6eb7393305d3f463db
Jan-Niklas Schäfer <Jan-NiklasSchaefer@gmx.de>
2019-09-23 09:15:39 +0200

    added helper class for validating identifier names

---
+++ ThinkSharp.FormulaParser/Validation/ValidationHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ThinkSharp.FormulaParsing.Validation
+{
+    public static class ValidationHelper
+    {
+        private static Regex variableRegex = new Regex("[a-zA-Z$_][a-zA-Z0-9$_]*", RegexOptions.Compiled);
+
+        public static bool IsValidIdentifier(string name) => variableRegex.IsMatch(name);
+    }
+}
